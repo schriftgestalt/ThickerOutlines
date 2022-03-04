370c6dba56dfd2617ba664ab47cbbfeaf4baeb7a
schriftgestalt <georg.seifert@mac.com>
2022-03-04 17:17:58 +0100

    Glyphs 3

--- Thicker Outlines.glyphsReporter/Contents/Resources/plugin.py
+++ Thicker Outlines.glyphsReporter/Contents/Resources/plugin.py
@@ -20,27 +20,24 @@ class ThickerOutlines(ReporterPlugin):
 
 	@objc.python_method
 	def settings(self):
-		self.menuName = Glyphs.localize({
-			'en': 'Thicker Outlines',
-			})
+		self.menuName = Glyphs.localize({'en': 'Thicker Outlines', 'de': 'Dicke Outlines'})
 
 	@objc.python_method
 	def foreground(self, layer):
 		graphicView = self.controller.graphicView()
-		print(graphicView.backgroundColor(), graphicView.foregroundColor())
-		graphicView.backgroundColor().set()
+		#print(graphicView.backgroundColor(), graphicView.foregroundColor())
+		graphicView.canvasColor().set()
 		bezierPath = layer.completeBezierPath
 		openBezierPath = layer.completeOpenBezierPath
 		bezierPath.setLineWidth_(2.25 / self.getScale())
-		bezierPath.stroke()
 		openBezierPath.setLineWidth_(2.25 / self.getScale())
-		openBezierPath.stroke()
-		graphicView.foregroundStrokeColor().set()
-		bezierPath.setLineWidth_(1.5 / self.getScale())
 		bezierPath.stroke()
-		openBezierPath.setLineWidth_(1.5 / self.getScale())
 		openBezierPath.stroke()
-		
+		graphicView.foregroundColor().set()
+		bezierPath.setLineWidth_(1.5 / self.getScale())
+		openBezierPath.setLineWidth_(1.5 / self.getScale())
+		bezierPath.stroke()
+		openBezierPath.stroke()
 
 	@objc.python_method
 	def __file__(self):
